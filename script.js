4ce7a3e7f8df2d653edbeacb3294ff31f5d13eba
24pa1a0571-ai <24pa1a0571@vishnu.edu.in>
2026-03-27 22:19:59 +0530

    Implement task management functionality in script.js
    Added script.js to handle core functionality of the Smart Study Planner app . Includes logic for adding  tasks dynamically to the list using JavaScript.

---
+++ script.js
@@ -0,0 +1,57 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function displayTasks() {
+    let list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    let completedCount = 0;
+
+    tasks.forEach((task, index) => {
+        let li = document.createElement("li");
+        li.textContent = task.text + " - " + task.date;
+
+        if (task.completed) {
+            li.classList.add("completed");
+            completedCount++;
+        }
+
+        li.onclick = function () {
+            toggleTask(index);
+        };
+
+        list.appendChild(li);
+    });
+
+    document.getElementById("progress").textContent =
+        "Progress: " + completedCount + "/" + tasks.length + " completed";
+}
+
+function addTask() {
+    let text = document.getElementById("taskInput").value;
+    let date = document.getElementById("dateInput").value;
+
+    if (text === "" || date === "") {
+        alert("Please enter task and date");
+        return;
+    }
+
+    tasks.push({ text: text, date: date, completed: false });
+
+    saveData();
+    displayTasks();
+
+    document.getElementById("taskInput").value = "";
+    document.getElementById("dateInput").value = "";
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    saveData();
+    displayTasks();
+}
+
+function saveData() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+displayTasks();
